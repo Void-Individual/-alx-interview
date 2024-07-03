ad414dfd40ca4107e24b445f5c27890a0d8bd03d
Void-Individual <otayomitv@gmail.com>
2024-07-03 19:47:31 +0100

    temp

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,7 @@
-#usr/bin/python
+#!/usr/bin/python3
 """ Module to implement lockbox techniques
 """
+
 
 def canUnlockAll(boxes):
     box_keys = [0] * len(boxes)
@@ -13,19 +14,20 @@ def canUnlockAll(boxes):
     # Open all boxes as they unlock boxes
     for count in range(len(boxes)):
         prev_unlocked = unlocked_boxes
-        print(prev_unlocked)
         for box in range(len(boxes)):
             if not box_keys[box]:
                 continue
             keys = boxes[box]
             for x in keys:
-                box_keys[x] += 1
+                try:
+                    box_keys[x] += 1
+                except IndexError:
+                    continue
                 if box_keys[x] == 1:
                     unlocked_boxes += 1
-        if ((prev_unlocked == unlocked_boxes) | (unlocked_boxes == len(boxes))):
+        if ((prev_unlocked == unlocked_boxes) |
+           (unlocked_boxes == len(boxes))):
             break
-
-    print(unlocked_boxes)
 
     if (0 in box_keys):
         return False
