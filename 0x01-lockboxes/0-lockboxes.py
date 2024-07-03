4422d48f53e295468e60600bc13db1f8a48f61db
Void-Individual <otayomitv@gmail.com>
2024-07-03 19:59:19 +0100

    Added coments

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -16,22 +16,28 @@ def canUnlockAll(boxes):
 
     # Open all boxes as they unlock boxes
     for count in range(len(boxes)):
+        # Set a value to use to monitor changes
         prev_unlocked = unlocked_boxes
         for box in range(len(boxes)):
+            # Check if this box has a key saved or not
             if not box_keys[box]:
                 continue
+            # Save all the keys in the box
             keys = boxes[box]
             for x in keys:
+                # Check if the key belongs to one of the boxes
                 try:
                     box_keys[x] += 1
                 except IndexError:
                     continue
+                # If a box gets a key for the first time, its unlocked
                 if box_keys[x] == 1:
                     unlocked_boxes += 1
+        # Check if all boxes have been open, or if none were opened
         if ((prev_unlocked == unlocked_boxes) |
            (unlocked_boxes == len(boxes))):
             break
-
+    # If any of the boxes haven't been opened
     if (0 in box_keys):
         return False
     return True
