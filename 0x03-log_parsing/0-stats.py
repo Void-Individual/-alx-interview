70f7854656eaf659170981063c2cddedd0854d35
Void-Individual <otayomitv@gmail.com>
2024-07-18 16:08:39 +0100

    Made a lot of modifications replaced except with finally

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 """Script to read stdin line by line and compute metrics"""
 
-from sys import stdin
+from sys import stdin, stdout
 import ipaddress
 from datetime import datetime
 
+def print_output():
+    """Function to print output"""
+
+    print(f"File size: {file_size}")
+    for key, value in status_codes.items():
+        if value > 0:
+            print(f"{key}: {value}")
+    stdout.flush()
 
 if __name__ == "__main__":
     x = 1
@@ -15,58 +23,50 @@ if __name__ == "__main__":
         '405': 0, '500': 0}
 
     # Toggle a loop to keep reading from stdin
-    while (x):
-        try:
-            # Read the passed line and confirm its format
-            data = stdin.readline()
+    try:
+        # Read the passed line and confirm its format
+        for line in stdin:
             # Retrieve the ip and confirm its validity
-            ip = data.split(' ', maxsplit=1)[0]
+            ip = line.split(' ', maxsplit=1)[0]
             try:
                 ipaddress.ip_address(str(ip))
                 # Collect the rest of the line minus ip
-                data = data.split('-', maxsplit=1)[1]
+                line = line.split('-', maxsplit=1)[1]
             except ipaddress.AddressValueError:
                 continue
 
             # Retrieve and check the date sub-string format
-            start_date = data.find('[')
-            end_date = data.find(']')
-            date = data[start_date + 1: end_date].strip()
+            start_date = line.find('[')
+            end_date = line.find(']')
+            date = line[start_date + 1: end_date].strip()
             try:
                 datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f")
-                # Retrieve the rest of the data without the date
-                data = data.split("]")[1]
+                # Retrieve the rest of the line without the date
+                line = line.split("]")[1]
             except ValueError:
                 continue
 
             # Retrieve and confirm the rest of the variables
             try:
                 # Confirm the presence of the required get response
-                data = data.split('\"GET /projects/260 HTTP/1.1\"')[1]
+                line = line.split('\"GET /projects/260 HTTP/1.1\"')[1]
                 # Retrieve the status code
-                status_code = data.split()[0]
+                status_code = line.split()[0]
                 # Add it to the stored dict keys if it exists
                 if status_code in status_codes:
                     status_codes[status_code] += 1
                 # Retrieve the size of the file
-                size = data.split()[-1]
+                size = line.split()[1]
+                # print(status_code, size)
                 # Confirm it to be an int and add it to the cumulative
                 file_size += int(size)
+                # print(x)
+                if x == 10:
+                    print_output()
+                    x = 1
+                else:
+                    x += 1
             except Exception:
                 continue
-
-        except KeyboardInterrupt:
-            print(f"File size: {file_size}")
-            for key, value in status_codes.items():
-                if value > 0:
-                    print(f"{key}: {value}")
-            break
-
-        if x >= 10:
-            print(f"File size: {file_size}")
-            for key, value in status_codes.items():
-                if value > 0:
-                    print(f"{key}: {value}")
-            x = 1
-        else:
-            x += 1
+    finally:
+        print_output()
