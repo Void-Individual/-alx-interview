93e16320ea0364e8a2d6d935e85c20f5e518be15
Void-Individual <otayomitv@gmail.com>
2024-07-27 16:58:19 +0100

    Checking hardope

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,11 +1,41 @@
-#!/usr/bin/python3
+# !/usr/bin/python3
 """Module containing a UTF-8 validator"""
 
 
 def validUTF8(data):
-    """Function to determine if data is a valid utf-8 recording"""
+    """Function to determine if given set of data is a valid utf-8 encoding"""
 
-    for num in data:
-        if num >= 128 or num < -128:
-            return False
-    return True
+    number_bytes = 0
+
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
+
+    # bin_data = {}
+    for i in data:
+        # bin_data[i] = bin(i)[2:]
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
+                return False
+
+        else:
+            if not (i & mask_1 and not (i & mask_2)):
+                # print(bin_data)
+                return False
+
+        number_bytes -= 1
+
+    # print(bin_data)
+
+    if number_bytes == 0:
+        return True
+
+    return False
