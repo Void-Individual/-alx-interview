a0e58900f99fc50d6cb42cc9d349065dfde9db28
Void-Individual <otayomitv@gmail.com>
2024-07-27 18:16:53 +0100

    Added comments

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,35 +7,55 @@ def validUTF8(data):
 
     number_bytes = 0
 
-    mask_1 = 1 << 7
-    mask_2 = 1 << 6
+    # Most significant first bit of a byte
+    MSB_1 = 1 << 7
+    # Second most significant bit of a byte
+    MSB_2 = 1 << 6
 
-    # bin_data = {}
     for i in data:
-        # bin_data[i] = bin(i)[2:]
+        # Reset default MSB for this value
         mask_byte = 1 << 7
 
+        # If this is not a continuation byte
         if number_bytes == 0:
+            # Compare the most significant bits of the byte for
+            # everytime it matches
             while mask_byte & i:
+                """This loop will only activate if the value in binary
+                is at least 10000000"""
+                # Increment the number for the number of bytes
                 number_bytes += 1
+                # Bit shift to check the value of the next bit of the value
                 mask_byte = mask_byte >> 1
+                # This way, you wiil count the number of bits in i
 
             if number_bytes == 0:
+                """This means the value is not a continuation byte,
+                or part of a multibyte value"""
                 continue
+                # Move on to the next
 
             if number_bytes == 1 or number_bytes > 4:
+                """If it is 1, it means its an invalid continuation byte that
+                should not be placed here. If greater than 4, then it has gone
+                beyond the 4-byte encoding system of utf8 and is invalid"""
                 return False
 
         else:
-            if not (i & mask_1 and not (i & mask_2)):
-                # print(bin_data)
+            """This byte is supposed to be the continuation byte of a
+            multi-byte value..."""
+            if not (i & MSB_1 and not (i & MSB_2)):
+                """Check if the first significant bit is 1, followed by 0,
+                if not, its invalid"""
                 return False
-
+        """If there are more than 1 bytes, decrement before looping again.
+        If its the end of a continuation byte,
+        decrement to 0 to start counting again"""
         number_bytes -= 1
 
-    # print(bin_data)
-
+    """AT the end of the for loop, if there was no byte unaccounted for,
+    the dataset passes"""
     if number_bytes == 0:
         return True
-
+    # Then a byte has been left trailing with no follow-up
     return False
