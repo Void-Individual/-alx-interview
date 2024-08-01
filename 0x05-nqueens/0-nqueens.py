48f66b1854a7f28c1dc8615c53bf32e3eff5c2b5
Void-Individual <otayomitv@gmail.com>
2024-08-01 14:46:42 +0100

    Incomplete nqueens function

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Module to solve the n queens puzzle"""
+
+import sys
+
+
+def nqueens(n):
+    """Function to place n non attacking queens on an NxN chessboard"""
+
+    pass
+
+
+if __name__ == "__main__":
+    # Retrieve the passed values
+    name = sys.argv[0]
+    count = len(sys.argv)
+
+    # If the wrong number of values is passed, abort
+    if count != 2:
+        print(f"Usage: {name} N")
+        sys.exit(1)
+
+    # Retrieve N and confirm it to be an int
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    nqueens(n)
