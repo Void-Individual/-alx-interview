2fcefbcb085a6f633e2cb64e28f7af8dcb98208f
Void-Individual <otayomitv@gmail.com>
2024-08-05 00:44:17 +0100

    Temp fix

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -4,20 +4,64 @@
 import sys
 
 
+def markTiles(n, tiles, queen):
+    """Function to mark unpassable tiles"""
+
+    # Horizontal
+    x = queen[0]
+    y = queen[1]
+
+    horizontal = tiles[x]
+
+    for a in range(n):
+        tiles[a][y] = 1
+        for tile in tiles:
+            print(tile)
+        print("Gap")
+        # horizontal[a] = 1
+        # print(horizontal)
+        # print(tiles)
+    # print(tiles)
+    tiles[x] = horizontal
+    # print(tiles)
+
+    for _ in range(n):
+        tiles[x][y] = 1
+
+    # for tile in tiles:
+    #    print(tile)
+
+
 def nqueens(n):
     """Function to place n non attacking queens on an NxN chessboard"""
 
-    pass
+    tiles = [[0] * n] * n
+    tiles[0][1] = 1
+    for tile in tiles:
+        print(tile)
+    tiles = [[0] * n] * n
+
+    queens = []
+    for x in range(1):
+        queen = []
+        queen.append(x)
+        queen.append(1)
+        markTiles(n, tiles, queen)
+
+        # for y in range(n):
+        #    queen.append(y)
+        # queens.append(queen)
+    # print(queens)
 
 
 if __name__ == "__main__":
     # Retrieve the passed values
-    name = sys.argv[0]
+    # name = sys.argv[0]
     count = len(sys.argv)
 
     # If the wrong number of values is passed, abort
     if count != 2:
-        print(f"Usage: {name} N")
+        print(f"Usage: nqueens N")
         sys.exit(1)
 
     # Retrieve N and confirm it to be an int
