f42c94b2be78974de354b0f70a060c56f2cfe598
Void-Individual <otayomitv@gmail.com>
2024-08-26 21:00:28 +0100

    Island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Module to find the perimeter of a grid island"""
+
+from typing import List
+
+
+def island_perimeter(grid: List[List[int]]) -> int:
+    """Function to return the perimeter of the island described in grid
+    which is a list of list of integers"""
+
+    perimeter = 0
+
+    # Set a range restriction to avoid the top and bottom waters
+    for row in range(1, len(grid) - 1):
+        # Set range to avoid leftmost and rightmost waters
+        for column in range(1, len(grid[row]) - 1):
+            # Check if the cell is a land square
+            if grid[row][column] == 1:
+                if grid[row - 1][column] == 0:
+                    perimeter += 1
+
+                if grid[row + 1][column] == 0:
+                    perimeter += 1
+
+                if grid[row][column - 1] == 0:
+                    perimeter += 1
+
+                if grid[row][column + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
